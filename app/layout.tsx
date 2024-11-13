c6b2e0e3e3a8be19da7b2e71373fc06eef1f2cb1
Hossein Rezaei <rezaeig22@gmail.com>
2024-11-13 14:19:42 +0330

    feat: customize radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
 import type { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import { Open_Sans } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
-import "./globals.css";
 import NavBar from "@/app/nav-bar";
 
-const openSans = Open_Sans({ subsets: ["latin"] });
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-open-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,8 +21,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={openSans.className}>
-        <Theme>
+      <body className={openSans.variable}>
+        <Theme accentColor="violet" appearance="light">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
