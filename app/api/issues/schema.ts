1f7adfb8541587fd5ecf78cfcc704f1dc68e08b6
Hossein Rezaei <rezaeig22@gmail.com>
2024-11-13 15:37:02 +0330

    feat: handling create issue errors

--- app/api/issues/schema.ts
+++ app/api/issues/schema.ts
@@ -1,8 +1,14 @@
 import { z } from "zod";
 
 export const createIssueSchema = z.object({
-  title: z.string().min(1).max(177),
-  description: z.string().min(1).max(177),
+  title: z //
+    .string()
+    .min(1, "Title is required.")
+    .max(200, "Title is too long."),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(200, "Description is too long."),
 });
 
 export type CreateIssueDto = z.infer<typeof createIssueSchema>;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, TextField, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
   description: string;
+}
+
+interface ErrorForm {
+  title?: string[];
+  description?: string[];
 }
 
 export default function NewIssuePage() {
@@ -17,9 +23,15 @@ export default function NewIssuePage() {
 
   const { register, control, handleSubmit } = useForm<IssueForm>();
 
+  const [errors, setErrors] = useState<ErrorForm>({} as ErrorForm);
+
   async function handleCreateIssue(data: IssueForm) {
-    await axios.post("/api/issues", data);
-    router.push("/issues");
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (err: any) {
+      setErrors(err.response.data.errors);
+    }
   }
 
   return (
@@ -28,13 +40,27 @@ export default function NewIssuePage() {
       onSubmit={handleSubmit(handleCreateIssue)}
     >
       <TextField.Root placeholder="Title" {...register("title")} />
+      {errors.title &&
+        errors.title.map((msg, i) => (
+          <Callout.Root key={i} color="red">
+            <Callout.Text>{msg}</Callout.Text>
+          </Callout.Root>
+        ))}
+
       <Controller
         name="description"
         control={control}
         render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
+          <SimpleMDE placeholder="Description" {...field} className="p-0" />
         )}
       />
+      {errors.description &&
+        errors.description.map((msg, i) => (
+          <Callout.Root key={i} color="red">
+            <Callout.Text>{msg}</Callout.Text>
+          </Callout.Root>
+        ))}
+
       <Button>Submit New Issue</Button>
     </form>
   );
