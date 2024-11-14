9ad3836ac06190528a694ed3bbcb0f72f83fa439
Hossein Rezaei <rezaeig22@gmail.com>
2024-11-15 02:12:13 +0330

    fix: issue edit button styles

--- app/issues/[id]/issue-edit-button.tsx
+++ app/issues/[id]/issue-edit-button.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 
 export default function IssueEditButton({ issueId }: { issueId: number }) {
   return (
-    <Button>
-      <Pencil2Icon />
-      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
-    </Button>
+    <Link href={`/issues/${issueId}/edit`}>
+      <Button color="green">
+        <Pencil2Icon />
+        Edit Issue
+      </Button>
+    </Link>
   );
 }
