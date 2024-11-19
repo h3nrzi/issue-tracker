a6d29db51ebd34a9063afe19f869b82c5ab3501b
Hossein Rezaei <rezaeig22@gmail.com>
2024-11-19 18:58:59 +0330

    refactor: issue toolbar

--- app/issues/[id]/issue-toolbar.tsx
+++ app/issues/[id]/issue-toolbar.tsx
@@ -10,6 +10,26 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function IssueToolbar({ issueId }: { issueId: number }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <EditButton issueId={issueId} />
+      <DeleteButton issueId={issueId} />
+    </div>
+  );
+}
+
+function EditButton({ issueId }: { issueId: number }) {
+  return (
+    <Link href={`/issues/edit/${issueId}`}>
+      <Button color="blue" style={{ width: "100%", cursor: "pointer" }}>
+        <Pencil2Icon />
+        Edit
+      </Button>
+    </Link>
+  );
+}
+
+function DeleteButton({ issueId }: { issueId: number }) {
   const router = useRouter();
   const [isDeleting, setIsDeleting] = useState(false);
   const [errors, setErrors] = useState({} as any);
@@ -28,51 +48,38 @@ export default function IssueToolbar({ issueId }: { issueId: number }) {
   }
 
   return (
-    <div className="flex flex-col gap-2">
-      <Link href={`/issues/edit/${issueId}`}>
-        <Button color="blue" style={{ width: "100%", cursor: "pointer" }}>
-          <Pencil2Icon />
-          Edit
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" style={{ cursor: "pointer" }} disabled={isDeleting}>
+          {isDeleting && <Spinner />}
+          <TrashIcon />
+          Delete
         </Button>
-      </Link>
-
-      <AlertDialog.Root>
-        <AlertDialog.Trigger>
-          <Button
-            color="red"
-            style={{ cursor: "pointer" }}
-            disabled={isDeleting}
-          >
-            {isDeleting && <Spinner />}
-            <TrashIcon />
-            Delete
-          </Button>
-        </AlertDialog.Trigger>
-        <AlertDialog.Content>
-          <AlertDialog.Title>Confirm Deleting</AlertDialog.Title>
-          <AlertDialog.Description>
-            Are you sure you want to delete this issue? This action cannot be
-            undone.
-          </AlertDialog.Description>
-          <div className="flex gap-3 mt-4">
-            <AlertDialog.Cancel>
-              <Button color="gray" variant="soft" style={{ cursor: "pointer" }}>
-                <MdCancel />
-                Cancel
-              </Button>
-            </AlertDialog.Cancel>
-            <AlertDialog.Action>
-              <Button
-                style={{ cursor: "pointer" }}
-                onClick={() => handleDeleteIssue(issueId)}
-              >
-                <IoIosCheckmarkCircle />
-                Delete Issue
-              </Button>
-            </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Root>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deleting</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <div className="flex gap-3 mt-4">
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft" style={{ cursor: "pointer" }}>
+              <MdCancel />
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              style={{ cursor: "pointer" }}
+              onClick={() => handleDeleteIssue(issueId)}
+            >
+              <IoIosCheckmarkCircle />
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </div>
+      </AlertDialog.Content>
 
       <AlertDialog.Root open={errors?.other}>
         <AlertDialog.Content>
@@ -89,6 +96,6 @@ export default function IssueToolbar({ issueId }: { issueId: number }) {
           </AlertDialog.Cancel>
         </AlertDialog.Content>
       </AlertDialog.Root>
-    </div>
+    </AlertDialog.Root>
   );
 }
