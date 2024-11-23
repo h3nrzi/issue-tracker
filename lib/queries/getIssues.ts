06ec096bd3b13aac898ff50fb51af547a3a0d178
Hossein Rezaei <rezaeig22@gmail.com>
2024-11-24 02:59:47 +0330

    feat: sort issues

--- lib/queries/getIssues.ts
+++ lib/queries/getIssues.ts
@@ -1,24 +1,35 @@
 import prisma from "@/prisma/client";
-import { Issue } from "@prisma/client";
-import { z } from "zod";
+import {Issue} from "@prisma/client";
+import {z} from "zod";
+import IssuesQuery from "@/types/IssuesQuery";
 
 const statusSchema = z.enum(["OPEN", "CLOSED", "IN_PROGRESS"]);
+const orderBySchema = z.enum(["title", "status", "createdAt"]);
+const orderDirectionSchema = z.enum(["asc", "desc"]);
 
-interface Query {
-  status?: string;
-}
-
-async function getIssues(query: Query): Promise<Issue[]> {
+async function getIssues(query: IssuesQuery): Promise<Issue[]> {
   if (query.status) {
     const validatedStatus = statusSchema.safeParse(query.status);
-    // if (!validatedStatus.success) throw new Error(`Invalid status: ${query.status}`);
     if (!validatedStatus.success) query.status = undefined;
+  }
+
+  if (query.orderBy) {
+    const validatedOrderBy = orderBySchema.safeParse(query.orderBy);
+    if (!validatedOrderBy.success) query.orderBy = undefined;
+  }
+
+  if (query.orderDirection) {
+    const validatedOrderDirection = orderDirectionSchema.safeParse(query.orderDirection);
+    if (!validatedOrderDirection.success) query.orderDirection = undefined;
   }
 
   return prisma.issue.findMany({
     where: {
       status: query.status,
     },
+    orderBy: {
+      [query.orderBy!]: query.orderDirection,
+    }
   });
 }
 
