01489d02c3864d08b1e4fff72cd39a195d517875
Hossein Rezaei <rezaeig22@gmail.com>
2024-11-27 02:01:17 +0330

    chore: changing nextauth middleware authorize action

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
-export { default } from "next-auth/middleware";
+import withAuth from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: { authorized: ({ token }) => !!token, },
+})
 
 export const config = {
   matcher: ["/issues/new", "/issues/edit/:id+"],
